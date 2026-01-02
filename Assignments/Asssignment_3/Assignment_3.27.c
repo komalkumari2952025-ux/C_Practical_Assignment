dab5f765c9a98d4dde92c9d2b6a7bc2bebb0a07f
Komal kumari <komalkumari2952025@gmail.com>
2026-01-02 23:40:14 +0530

    Add files via upload

---
+++ Assignments/Asssignment_3/Assignment_3.27.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 0; i < 7; i++) {
+
+        
+        for (j = 0; j < 5; j++) {
+            if (j == 0 || j == 4 - i || j == i - 2)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("  ");
+
+        
+        for (j = 0; j < 5; j++) {
+            if ((i == 0 || i == 6) && j > 0 && j < 4)
+                printf("*");
+            else if ((j == 0 || j == 4) && i > 0 && i < 6)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("  ");
+
+       
+        for (j = 0; j < 5; j++) {
+            if (j == 0 || j == 4 || (i == j && i < 3) || (i + j == 4 && i < 3))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("  ");
+
+        
+        for (j = 0; j < 5; j++) {
+            if ((i == 0 && j > 0 && j < 4) ||
+                ((j == 0 || j == 4) && i > 0) ||
+                (i == 3))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("  ");
+
+        
+        for (j = 0; j < 5; j++) {
+            if (j == 0 || i == 6)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
